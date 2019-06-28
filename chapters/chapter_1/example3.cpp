0baa044bf5f8214d7cc6c6cb6332338938fc4b2f
allsolu <lukeallsopp93@gmail.com>
2019-06-28 14:06:20 +1000

    new file:   example3.cpp

---
+++ chapters/chapter_1/example3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	//ask for a persons name
+	std::cout << "Please enter your first name: ";
+
+	//read the name
+	std::string name;
+	std::cin >> name;
+
+	//build the message we intend to write
+	const std::string greeting = "Hello, " + name + " ya fuck";
+	
+
+	//TBD
+	
+		
+
+	return 0; 
+	
+}
+
+
+
